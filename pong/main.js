548ebbcb1cd7f39ba4b079c8a71412b5f87a73f2
facundobonanata <gonzaloblmnt@gmail.com>
2022-06-26 13:01:01 -0300

    Movimiento de barras y pelota con teclado

--- pong/main.js
+++ pong/main.js
@@ -4,6 +4,7 @@
         this.width = width;
         this.height = height;
         this.playing = false;
+        this.playing = false;
         this.game_over = false;
         this.bars = [];
         this.ball = null;
@@ -11,7 +12,7 @@
 
     self.Board.prototype = {
         get elements(){
-            var elements = this.bars;
+            var elements = this.bars.map(function(bar){ return bar; }); //copia del arreglo
             elements.push(this.ball);
             return elements;
         }       
@@ -19,21 +20,30 @@
 })();
 
 (function(){
-    self.Ball = function(x,y,radius,board){
+    self.Ball = function(x,y,radius,board){  //creacion pelota
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.speed_y = 0;
         this.speed_x = 3;
         this.board = board;
+        this.direction =1;
 
+        this.board=board;
         board.ball = this;
         this.kind = "circle";
-    }
-})();
 
+
+}
+    self.Ball.prototype = {
+    move: function(){
+        this.x += (this.speed_x * this.direction);
+        this.y += (this.speed_y);
+    }
+}
+})();
 (function(){
-    self.Bar =function(x,y,width,height,board){
+    self.Bar = function(x,y,width,height,board){ //crear barras
         this.x = x;
         this.y = y;
         this.width = width;
@@ -58,8 +68,7 @@
 })();
 
 (function(){
-    self.BoardView = function (canvas, board){
-    console.log(board);
+    self.BoardView = function (canvas, board){  //dibujar la pizarra
 
         this.canvas = canvas;
         this.canvas.width = board.width;
@@ -81,18 +90,23 @@
             };      
         },
         play: function(){
+            //console.log(ev.keyCode)
+          if(this.board.playing){
             this.clean();
             this.draw();
+            this.board.ball.move();
         }
+      }
     }
+    
+
 
     function draw(ctx, element){
         switch(element.kind){
-            case "rectangle":
-
+            case "rectangle":   //dibujando las barras
                 ctx.fillRect(element.x,element.y,element.width,element.height);
                  break;
-             case "circle":
+             case "circle":  //dibujando la pelota
                 ctx.beginPath();
                 ctx.arc(element.x,element.y,element.radius,0,7);
                 ctx.fill();
@@ -104,38 +118,43 @@
 })();
 
 var board = new Board(800,400);
-console.log(board)
 var bar = new Bar(20,100,15,80,board)
 var bar_2 = new Bar(765,100,15,80,board)
 var canvas = document.getElementById('canvas') //obtener elementos document del objt model
-var board_View = new BoardView(canvas, board);
+var board_view = new BoardView(canvas, board);
 var ball = new Ball(350,100,10,board)
 
 
 
-document.addEventListener("Keydown", function(ev){
-    ev.preventDefault(); //evitar movimiento de ventana
+document.addEventListener("keydown", function(ev){ //Movimiento de las barras con teclado
+    console.log(ev.keyCode);
     if(ev.keyCode==38){
+        ev.preventDefault(); //evitar movimiento de ventana
         bar.up();
-    }
-    else if(ev.keyCode == 40){
+    }else if(ev.keyCode == 40){
+        ev.preventDefault();
         bar.down();
-    }
-    else if(ev.keyCode == 87){
+    }else if(ev.keyCode == 87){
+        ev.preventDefault();
         //W
         bar_2.up();
-    }
-    else if(ev.keyCode == 40){
+    }else if(ev.keyCode == 83){
+        ev.preventDefault();
         //S
         bar_2.down();
-    }
+    }else if(ev.keyCode === 32){
+        ev.preventDefault();
+        board.playing = !board.playing;
+    }   
 });
 
+board_view.draw();
 //window.addEventListener("load", main);
 window.requestAnimationFrame(controller)//muevete al siguiente frame y utliza la funcion controller
 
+
 function controller(){
-    board_View.play();
-    window.requestAnimationFrame(controller)
+    board_view.play();
+    requestAnimationFrame(controller);
 
 }
