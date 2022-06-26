e8e9c993893065168b04402239ac1b04445292f4
facundobonanata <gonzaloblmnt@gmail.com>
2022-06-26 10:53:07 -0300

    Actualizacion construyendo barras

--- pong/main.js
+++ pong/main.js
@@ -19,6 +19,27 @@
 })();
 
 (function(){
+    self.Bar =function(x,y,width,height,board){
+        this.x=x;
+        this.y=y
+        this.width=width
+        this.height=height;
+        this.board=board;
+        this.board.bars.push(this);
+        this.kind ="rectangle";
+    }
+
+    self.Bar.prototype = {
+        down: function(){
+
+        },
+        up: function(){
+
+        }
+    }
+})();
+
+(function(){
     self.BoardView = function (canvas, board){
     console.log(board);
 
@@ -29,6 +50,27 @@
         this.ctx = canvas.getContext("2d"); //Obj a traves del cual podemos dibujar en js
 
     }
+
+    self.BoardView.prototype={
+        draw: function(){
+            for (var i = this.board.elements.length -1; i>=0; i--){
+               var el= this.board.elements[i]
+
+               draw(this.ctx,el)
+            }       
+        }
+    }
+
+    function draw(ctx, element){
+        if(element !== null && element.hasOwnProperty("kind")){
+            switch(element.kind){
+                case "rectangle":
+                     ctx.fillRect(element.x,element.y,element.width,element.height);
+                     break;            
+       }
+        
+    }
+}
 })();
 
 window.addEventListener("load", main);
@@ -36,7 +78,11 @@ window.addEventListener("load", main);
 function main(){
     var board = new Board(800,400);
     console.log(board)
+    var bar = new Bar(20,100,15,80,board)
+    var bar = new Bar(765,100,15,80,board)
+
     var canvas = document.getElementById('canvas') //obtener elementos document del objt model
     var board_View = new BoardView(canvas, board);
+    board_View.draw();
 
 }
